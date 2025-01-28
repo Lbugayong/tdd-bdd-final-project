e07ac4da8a7e50d3b05c971e4177fad591bf650c
Lbugayong <lisa.bugayong@gmail.com>
2025-01-27 19:42:10 -0800

    Changes to final project

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -32,13 +32,22 @@ from selenium.webdriver.support import expected_conditions
 
 ID_PREFIX = 'product_'
 
-
 @when('I visit the "Home Page"')
 def step_impl(context):
     """ Make a call to the base URL """
     context.driver.get(context.base_url)
     # Uncomment next line to take a screenshot of the web page
     # context.driver.save_screenshot('home_page.png')
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
 
 @then('I should see "{message}" in the title')
 def step_impl(context, message):
@@ -104,7 +113,10 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
 
 ##################################################################
 # This code works because of the following naming convention:
@@ -112,6 +124,12 @@ def step_impl(context, element_name):
 # prefixed by ID_PREFIX so the Name field has an id='pet_name'
 # We can then lowercase the name and prefix with pet_ to get the id
 ##################################################################
+
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text)
+
 
 @then('I should see "{text_string}" in the "{element_name}" field')
 def step_impl(context, text_string, element_name):
